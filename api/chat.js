64949049e1e7ef121918d70502408da73fd8c23e
JNUZXF <30730585+JNUZXF@users.noreply.github.com>
2023-07-16 03:24:19 +0800

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,70 +1,43 @@
-const express = require('express');
-const path = require('path');
-const fetch = require('node-fetch');
-const bodyParser = require('body-parser');
+// chat.js
+import fetch from 'node-fetch';
 
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.use(express.static('public'));
-app.use(bodyParser.json());
-
-app.post('/api/chat', async (req, res) => {
+export default async (req, res) => {
   const { messages } = req.body;
-  const model = 'gpt-3.5-turbo';
-  const baseURL = 'api.openai.com';
-  const key = process.env.OPENAI_API_KEY;
 
-  try {
-    const response = await fetch(`https://${baseURL}/v1/chat/completions`, {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${key}`,
-      },
-      method: 'POST',
-      body: JSON.stringify({
-        model,
-        messages: messages.map(message => ({
-          role: message.role,
-          content: message.content,
-        })),
-        temperature: 0.5,
-        stream: true,
-      }),
-    });
+  const response = await fetch('https://api.openai.com/v1/chat/completions', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+    },
+    body: JSON.stringify({
+      model: 'gpt-3.5-turbo',
+      messages: [...messages, { role: 'system', content: 'You are a helpful assistant.' }],
+      stream: true,
+    })
+  });
 
-    if (!response.ok) {
-      res.status(response.status).json(response.body);
-    }
-
-    const stream = new ReadableStream({
-      start(controller) {
-        const decoder = new TextDecoder();
-        const encoder = new TextEncoder();
-        response.body
-          .on('data', data => {
-            controller.enqueue(encoder.encode(data));
-          })
-          .on('end', () => {
-            controller.close();
-          });
-      },
-    });
-
-    const reader = stream.getReader();
-    let result = '';
-    while (true) {
-      const { done, value } = await reader.read();
-      if (done) break;
-      result += new TextDecoder().decode(value);
-    }
-    res.send(JSON.parse(result));
-  } catch (error) {
-    console.error(error);
-    res.sendStatus(500);
+  if (!response.ok) {
+    res.status(response.status).json(await response.text());
+    return;
   }
-});
 
-module.exports = app;
+  res.set({
+    'Content-Type': 'text/plain',
+    'Transfer-Encoding': 'chunked'
+  });
 
+  const decoder = new TextDecoder();
+  const reader = response.body.getReader();
+  let data;
 
+  while (!(data = await reader.read()).done) {
+    const text = decoder.decode(data.value);
+    const match = text.match(/"content":"(.*?)"/);
+    const content = match ? match[1] : '';
+
+    res.write(content);
+  }
+
+  res.end();
+};
