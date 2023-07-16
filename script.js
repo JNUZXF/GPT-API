5dc456a2c1319c21c0cdd41b6a532dc17737d81b
JNUZXF <30730585+JNUZXF@users.noreply.github.com>
2023-07-16 16:17:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 document.getElementById('form').addEventListener('submit', function (event) {
     event.preventDefault();
-  
+
     const inputElement = document.getElementById('input');
     const messagesElement = document.getElementById('messages');
-  
+
     const userMessage = inputElement.value;
     inputElement.value = '';
-  
+
     messagesElement.innerHTML += `<div>User: ${userMessage}</div>`;
-  
+
     fetch('/api/chat', {
       method: 'POST',
       headers: {
@@ -16,32 +16,9 @@ document.getElementById('form').addEventListener('submit', function (event) {
       },
       body: JSON.stringify({ messages: [{role: 'user', content: userMessage}] })
     })
-      .then(response => response.body)
-      .then(rb => {
-        const reader = rb.getReader();
-  
-        return new ReadableStream({
-          start(controller) {
-            function push() {
-              reader.read().then(({done, value}) => {
-                if (done) {
-                  controller.close();
-                  return;
-                }
-                controller.enqueue(value);
-                push();
-              });
-            };
-  
-            push();
-          }
-        });
-      })
-      .then(stream => {
-        return new Response(stream, { headers: { 'Content-Type': 'text/html' } }).text();
-      })
+      .then(response => response.json()) // Get the result as JSON
       .then(result => {
-        messagesElement.innerHTML += `<div>Bot: ${result}</div>`;
+        const botMessage = result.choices[0].message.content; // Extract the bot message from the result
+        messagesElement.innerHTML += `<div>Bot: ${botMessage}</div>`; // Display the bot message
       });
-  });
-  
+});
