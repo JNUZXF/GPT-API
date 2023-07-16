c6db5785219bec802a478ac3bf95324684762918
JNUZXF <30730585+JNUZXF@users.noreply.github.com>
2023-07-16 16:56:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -16,9 +16,14 @@ document.getElementById('form').addEventListener('submit', function (event) {
       },
       body: JSON.stringify({ messages: [{role: 'user', content: userMessage}] })
     })
-      .then(response => response.json()) // Get the result as JSON
+      .then(response => response.json())
       .then(result => {
-        const botMessage = result.choices[0].message.content; // Extract the bot message from the result
-        messagesElement.innerHTML += `<div>Bot: ${botMessage}</div>`; // Display the bot message
+        if (result.choices && result.choices.length > 0) {
+          const botMessage = result.choices[0].message.content;
+          messagesElement.innerHTML += `<div>Bot: ${botMessage}</div>`;
+        } else {
+          // Handle the case where there is no bot message
+          console.error('No bot message received', result);
+        }
       });
 });
