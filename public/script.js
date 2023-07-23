7c11846bc2240be415bb3e91ea94ce33f885b9d0
JNUZXF <30730585+JNUZXF@users.noreply.github.com>
2023-07-23 18:47:59 +0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -13,45 +13,46 @@ document.getElementById('form').addEventListener('submit', function (event) {
     messagesElement.appendChild(contentElement);
 
     fetch('/api/chat', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ messages: [{role: 'user', content: userMessage}] })
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ messages: [{ role: 'user', content: userMessage }] })
     })
-      .then(response => {
-        const reader = response.body.getReader();
-        const decoder = new TextDecoder('utf-8');
-        let buffer = '';
+        .then(response => {
+            const reader = response.body.getReader();
+            const decoder = new TextDecoder('utf-8');
+            let buffer = '';
 
-        function processEvent(event) {
-          console.log(event);  // Print the event data
-          try {
-            const data = JSON.parse(event);
-            contentElement.textContent += data.choices[0].message.content;
-          } catch (error) {
-            console.error('Error parsing JSON', error);
-          }
-        }
-
-        function processText({ done, value }) {
-          buffer += decoder.decode(value, { stream: true });
-          let start = 0;
-          let end;
-          while ((end = buffer.indexOf('\n', start)) !== -1) {
-            const event = buffer.slice(start, end).trim();
-            if (event.startsWith('data: ')) {
-              processEvent(event.slice(6).trim());
+            function processEvent(event) {
+                console.log(event);  // Print the event data
+                try {
+                    const data = JSON.parse(event);
+                    console.log(data);  // Print the parsed data
+                    contentElement.textContent += data.choices[0].delta.content;
+                } catch (error) {
+                    console.error('Error parsing JSON', error);
+                }
             }
-            start = end + 1;
-          }
-          buffer = buffer.slice(start);
 
-          if (!done) {
+            function processText({ done, value }) {
+                buffer += decoder.decode(value, { stream: true });
+                let start = 0;
+                let end;
+                while ((end = buffer.indexOf('\n\n', start)) !== -1) {
+                    const event = buffer.slice(start, end).trim();
+                    if (event.startsWith('data: ')) {
+                        processEvent(event.slice(6));
+                    }
+                    start = end + 2;
+                }
+                buffer = buffer.slice(start);
+
+                if (!done) {
+                    return reader.read().then(processText);
+                }
+            }
+
             return reader.read().then(processText);
-          }
-        }
-
-        return reader.read().then(processText);
-      });
+        });
 });
