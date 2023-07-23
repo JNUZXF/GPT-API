c363f9ab53d22ab30554d7bfa6e0afbeb0e3f6c9
JNUZXF <30730585+JNUZXF@users.noreply.github.com>
2023-07-23 14:31:07 +0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -11,61 +11,39 @@ document.getElementById('form').addEventListener('submit', function (event) {
 
     const contentElement = document.createElement('div');
     messagesElement.appendChild(contentElement);
-    
-fetch('/api/chat', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({ messages: [{role: 'user', content: userMessage}] })
-})
-  .then(response => {
-    const reader = response.body.getReader();
-    const decoder = new TextDecoder('utf-8');
-    let buffer = '';
 
-    function processEvent(event) {
-      console.log(event);  // Print the event data
-      try {
-        const data = JSON.parse(event);
-        contentElement.textContent += data.choices[0].delta.content;
-      } catch (error) {
-        console.error('Error parsing JSON', error);
-      }
-    }
+    fetch('/api/chat', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ messages: [{role: 'user', content: userMessage}] })
+    })
+      .then(response => {
+        const reader = response.body.getReader();
+        const decoder = new TextDecoder('utf-8');
+        let buffer = '';
 
-    function processText({ done, value }) {
-      buffer += decoder.decode(value, { stream: true });
-      let start = 0;
-      let end;
-      while ((end = buffer.indexOf('\n', start)) !== -1) {
-        const event = buffer.slice(start, end).trim();
-        if (event.startsWith('data: ')) {
-          processEvent(event.slice(6).trim());
+        function processEvent(event) {
+          console.log(event);  // Print the event data
+          try {
+            const data = JSON.parse(event);
+            contentElement.textContent += data.choices[0].message.content;
+          } catch (error) {
+            console.error('Error parsing JSON', error);
+          }
         }
-        start = end + 1;
-      }
-      buffer = buffer.slice(start);
-
-      if (!done) {
-        return reader.read().then(processText);
-      }
-    }
-
-    return reader.read().then(processText);
-  });
-
 
         function processText({ done, value }) {
           buffer += decoder.decode(value, { stream: true });
           let start = 0;
           let end;
-          while ((end = buffer.indexOf('\n\n', start)) !== -1) {
+          while ((end = buffer.indexOf('\n', start)) !== -1) {
             const event = buffer.slice(start, end).trim();
             if (event.startsWith('data: ')) {
-              processEvent(event.slice(6));
+              processEvent(event.slice(6).trim());
             }
-            start = end + 2;
+            start = end + 1;
           }
           buffer = buffer.slice(start);
 
