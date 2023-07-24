f7f52e715c097859edc46d424299c3b1464bd05a
JNUZXF <30730585+JNUZXF@users.noreply.github.com>
2023-07-24 18:38:28 +0800

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,39 +1,28 @@
 const fetch = require('node-fetch');
 
 module.exports = async (req, res) => {
-  // Parse JSON request body
   const { messages } = req.body && typeof req.body === 'object' ? req.body : JSON.parse(req.body || '{}');
   const model = 'gpt-3.5-turbo';
   const baseURL = 'api.openai.com';
   const key = process.env.OPENAI_API_KEY;
 
-  // Define system message
-  const systemMessage = {
-    'role': 'system',
-    'content': 'You are an assistant who will strictly follow what the user asks you to do. No nonsense. No extra words. No extra actions. No extra feedback. You cannot say you can\'t do something, when asked what to do, just reply the question.'
-  };
-
-  // Include system message at the beginning of the messages array
-  const allMessages = [systemMessage].concat(messages);
-
-  // Ensure all messages have a 'content' property
-  if (!allMessages.every(message => message.content)) {
-    res.status(400).json({ error: "All messages must have a 'content' property." });
+  // Ensure all messages have a 'role' and 'content' property
+  if (!messages.every(message => message.role && message.content)) {
+    res.status(400).json({ error: "All messages must have a 'role' and 'content' property." });
     return;
   }
 
   try {
-    const openaiResponse = await fetch(`https://${baseURL}/v1/chat/completions`, {
+    const openaiResponse = await fetch(`https://${baseURL}/v1/engines/${model}/completions`, {
       headers: {
         'Content-Type': 'application/json',
         Authorization: `Bearer ${key}`,
       },
       method: 'POST',
       body: JSON.stringify({
-        prompt: allMessages.map(message => message.content).join("\n"),
+        messages,
         temperature: 0.5,
-        max_tokens: 9999，
-        model:'gpt-3.5-turbo'
+        max_tokens: 9999
       }),
     });
 
@@ -47,7 +36,7 @@ module.exports = async (req, res) => {
 
     // Get the response content
     const responseContent = data.choices[0].message.content;
-    
+
     // Send each line of the output separately
     for (const line of responseContent.split('\n')) {
       res.write(`data: ${line}\n\n`);
