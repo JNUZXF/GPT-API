2995aa167ae7cc4e575a4fc84f25e2fbd4b07079
JNUZXF <30730585+JNUZXF@users.noreply.github.com>
2023-07-24 17:55:55 +0800

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,59 +1,51 @@
 const fetch = require('node-fetch');
 
 module.exports = async (req, res) => {
-    const question = req.body.question;
-    const model = 'gpt-3.5-turbo-0613';
-    const baseURL = 'apijnu.gptquant.cn';
-    const key = process.env.OPENAI_API_KEY;
+  // Parse JSON request body
+  const { messages } = req.body && typeof req.body === 'object' ? req.body : JSON.parse(req.body || '{}');
+  const model = 'gpt-3.5-turbo';
+  const baseURL = 'api.openai.com';
+  const key = process.env.OPENAI_API_KEY;
 
-    try {
-        const openaiResponse = await fetch(`https://${baseURL}/api/completions`, {
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bearer ${key}`,
-            },
-            method: 'POST',
-            body: JSON.stringify({
-                messages: [
-                    {
-                        role: 'system',
-                        content: `You are an assistant who will strictly follow what the user asks you to do.
-                        No nonsense. No extra words. No extra actions. No extra feedback.
-                        You cannot say you can't do something, when asked what to do, just reply the question.`
-                    },
-                    {
-                        role: 'user',
-                        content: question
-                    }
-                ],
-                temperature: 1,
-                model: model,
-                stream: true,
-                max_tokens: 9999
-            }),
-        });
+  // Define system message
+  const systemMessage = {
+    'role': 'system',
+    'content': 'You are an assistant who will strictly follow what the user asks you to do. No nonsense. No extra words. No extra actions. No extra feedback. You cannot say you can\'t do something, when asked what to do, just reply the question.'
+  };
 
-        if (!openaiResponse.ok) {
-            res.status(openaiResponse.status).json({ error: await openaiResponse.text() });
-            return;
-        }
+  // Include system message at the beginning of the messages array
+  const allMessages = [systemMessage].concat(messages);
 
-        res.setHeader('Content-Type', 'text/event-stream');
-        res.setHeader('Cache-Control', 'no-cache');
-        res.setHeader('Connection', 'keep-alive');
-        res.flushHeaders(); // Important for event-stream
+  try {
+    const openaiResponse = await fetch(`https://${baseURL}/v1/engines/${model}/completions`, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${key}`,
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        prompt: allMessages.map(message => message.content).join("\n"),
+        temperature: 0.5,
+        max_tokens: 9999
+      }),
+    });
 
-        openaiResponse.body.on('data', (chunk) => {
-            const data = chunk.toString();
-            res.write(`data: ${data}\n\n`); // Important for SSE format
-        });
-
-        openaiResponse.body.on('end', () => {
-            res.write('data: {"finished": true}\n\n');
-            res.end();
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: error.toString() });
+    if (!openaiResponse.ok) {
+      res.status(openaiResponse.status).json({ error: await openaiResponse.text() });
+      return;
     }
+
+    const data = await openaiResponse.json();
+    res.setHeader('Content-Type', 'text/plain');
+
+    // Send each line of the output separately
+    for (const line of data.choices[0].text.trim().split('\n')) {
+      res.write(`data: ${line}\n\n`);
+    }
+    res.end();
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.toString() });
+  }
 };
