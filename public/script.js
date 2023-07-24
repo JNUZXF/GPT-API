e32ccfe0d292f5ccbeacafb8fd32e523c0124c55
JNUZXF <30730585+JNUZXF@users.noreply.github.com>
2023-07-24 16:18:50 +0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -19,19 +19,50 @@ document.getElementById('form').addEventListener('submit', function (event) {
         },
         body: JSON.stringify({ messages: [{ role: 'user', content: userMessage }] })
     })
-        .then(response => response.text())
-        .then(data => {
-            const events = data.trim().split('\n');
-            for (let event of events) {
-                if (event.startsWith('data: ')) {
-                    const line = event.slice(6).trim();
-                    if (line.startsWith('{') && line.endsWith('}')) {
-                        const output = JSON.parse(line);
-                        if (output.role === 'system' || output.role === 'assistant') {
-                            contentElement.innerHTML += `<div>${output.role}: ${output.content}</div>`;
+        .then(response => {
+            const reader = response.body.getReader();
+            const decoder = new TextDecoder('utf-8');
+            let buffer = '';
+
+            function processEvent(line) {
+                const message = JSON.parse(line);
+                if (message.role === 'system') {
+                    return false;
+                }
+                handleNewMessage(contentElement, message.content);
+                return message.finished;
+            }
+
+            function handleNewMessage(contentElement, message) {
+                const paragraph = document.createElement('p');
+                paragraph.textContent = message;
+                contentElement.appendChild(paragraph);
+            }
+
+            function processText({ done, value }) {
+                buffer += decoder.decode(value, { stream: true });
+                let start = 0;
+                let end;
+                while ((end = buffer.indexOf('\n', start)) !== -1) {
+                    const event = buffer.slice(start, end).trim();
+                    if (event.startsWith('data: ')) {
+                        const line = event.slice(6).trim();
+                        if (line.startsWith('{') && line.endsWith('}')) {
+                            const isDone = processEvent(line);
+                            if (isDone) {
+                                break;
+                            }
                         }
                     }
+                    start = end + 1;
+                }
+                buffer = buffer.slice(start);
+
+                if (!done) {
+                    return reader.read().then(processText);
                 }
             }
+
+            return reader.read().then(processText);
         });
 });
