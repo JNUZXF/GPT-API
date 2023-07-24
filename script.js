2fe63af2bc1b2279160e67dc9da8bbb88428a99c
JNUZXF <30730585+JNUZXF@users.noreply.github.com>
2023-07-24 21:59:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -24,45 +24,54 @@ document.getElementById('form').addEventListener('submit', async (event) => {
   const reader = response.body.getReader();
   let lastMessage = '';
 
-  // This function handles the stream of data
-  async function readStream() {
-    const { value, done } = await reader.read();
-  
-    if (done) {
-      return;
-    }
-  
-    // Convert the Uint8Array to a string and add it to the last message
-    lastMessage += new TextDecoder().decode(value);
-  
-    // Check if the last message is complete (ends with two newline characters)
-    while (lastMessage.includes('\n\n')) {
-      // Find the first complete message
-      const endOfMessage = lastMessage.indexOf('\n\n');
-      const completeMessage = lastMessage.slice(0, endOfMessage);
-  
-      // Split the message into chunks by 'data: '
-      const chunks = completeMessage.split('data: ');
-  
-      // Parse each chunk and append the AI's message to the messages container
-      for (let i = 1; i < chunks.length; i++) {  // Skip the first chunk because it's always empty
-        const messageObj = JSON.parse(chunks[i]);
+// This function handles the stream of data
+async function readStream() {
+  const { value, done } = await reader.read();
+
+  if (done) {
+    return;
+  }
+
+  // Convert the Uint8Array to a string and add it to the last message
+  lastMessage += new TextDecoder().decode(value);
+
+  // Check if the last message is complete (ends with two newline characters)
+  while (lastMessage.includes('\n\n')) {
+    // Find the first complete message
+    const endOfMessage = lastMessage.indexOf('\n\n');
+    const completeMessage = lastMessage.slice(0, endOfMessage);
+
+    // Split the message into chunks by 'data: '
+    const chunks = completeMessage.split('\n\ndata: ');
+
+    // Parse each chunk and append the AI's message to the messages container
+    for (let i = 0; i < chunks.length; i++) {
+      const chunk = chunks[i];
+
+      // Only parse the chunk if it's a complete JSON object
+      if (chunk.endsWith('\n')) {
+        const messageObj = JSON.parse(chunk.slice(0, -1));
         const aiMessage = messageObj.choices[0].delta.content;
-  
+
         // Append the AI's message to the messages container, if it's not empty
         if (aiMessage) {
           messagesContainer.innerHTML += `<div class="message ai-message">${aiMessage}</div>`;
           window.scrollTo(0, document.body.scrollHeight);
         }
+      } else {
+        // If the chunk is not a complete JSON object, keep it for the next data event
+        lastMessage = 'data: ' + chunk;
       }
-  
-      // Start a new message with any remaining text
-      lastMessage = lastMessage.slice(endOfMessage + 2);
     }
-  
-    // Keep reading the stream
-    readStream();
+
+    // Start a new message with any remaining text
+    lastMessage = lastMessage.slice(endOfMessage + 2);
   }
+
+  // Keep reading the stream
+  readStream();
+}
+
 
   readStream().catch((error) => {
     console.error('Error while reading the response stream', error);
