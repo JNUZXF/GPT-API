8d20ca631f9369177a1b6f68fff73c66f954588a
JNUZXF <30730585+JNUZXF@users.noreply.github.com>
2023-07-24 19:55:01 +0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,34 +1,45 @@
-document.getElementById('form').addEventListener('submit', function (event) {
-    event.preventDefault();
+document.getElementById('form').addEventListener('submit', async (event) => {
+  event.preventDefault();
+  
+  const inputField = document.getElementById('input');
+  const messagesContainer = document.getElementById('messages');
+  
+  const message = {
+    role: 'system',
+    content: inputField.value,
+  };
+  
+  inputField.value = '';
+  messagesContainer.innerHTML += `<div class="message user-message">${message.content}</div>`;
 
-    const inputElement = document.getElementById('input');
-    const messagesElement = document.getElementById('messages');
+  const response = await fetch('/api/chat', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ messages: [message] }),
+  });
 
-    const userMessage = inputElement.value;
-    inputElement.value = '';
+  const reader = response.body.getReader();
 
-    messagesElement.innerHTML += `<div>User: ${userMessage}</div>`;
+  // This function handles the stream of data
+  async function readStream() {
+    const { value, done } = await reader.read();
 
-    const contentElement = document.createElement('div');
-    messagesElement.appendChild(contentElement);
+    if (done) {
+      return;
+    }
 
-    const es = new EventSource(`/api/chat?message=${encodeURIComponent(userMessage)}`);
+    // Convert the Uint8Array to a string and append it to the messages container
+    const text = new TextDecoder().decode(value);
+    messagesContainer.innerHTML += `<div class="message ai-message">${text}</div>`;
+    window.scrollTo(0, document.body.scrollHeight);
 
-    es.addEventListener('message', function (event) {
-        const message = event.data;
+    // Keep reading the stream
+    readStream();
+  }
 
-        if (message) {
-            const paragraph = document.createElement('p');
-            paragraph.textContent = message;
-            contentElement.appendChild(paragraph);
-        }
-    });
-
-    es.addEventListener('error', function () {
-        es.close();
-    });
-
-    es.addEventListener('end', function () {
-        es.close();
-    });
+  readStream().catch((error) => {
+    console.error('Error while reading the response stream', error);
+  });
 });
