4bfb88a31a4ba1d43ea3157be3588234cb228fec
JNUZXF <30730585+JNUZXF@users.noreply.github.com>
2023-07-24 16:43:14 +0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -17,52 +17,25 @@ document.getElementById('form').addEventListener('submit', function (event) {
         headers: {
             'Content-Type': 'application/json'
         },
-    body: JSON.stringify({ question: userMessage }) 
+        body: JSON.stringify({ question: userMessage })
     })
         .then(response => {
-            const reader = response.body.getReader();
-            const decoder = new TextDecoder('utf-8');
-            let buffer = '';
-
-            function processEvent(line) {
-                const message = JSON.parse(line);
-                if (message.role === 'system') {
-                    return false;
+            const eventSource = new EventSource(response.url);
+            eventSource.onmessage = (event) => {
+                const message = JSON.parse(event.data);
+                console.log(message); // Print out the message
+                if (message.role !== 'system') {
+                    handleNewMessage(contentElement, message.content);
                 }
-                handleNewMessage(contentElement, message.content);
-                return message.finished;
-            }
-
-            function handleNewMessage(contentElement, message) {
-                const paragraph = document.createElement('p');
-                paragraph.textContent = message;
-                contentElement.appendChild(paragraph);
-            }
-
-            function processText({ done, value }) {
-                buffer += decoder.decode(value, { stream: true });
-                let start = 0;
-                let end;
-                while ((end = buffer.indexOf('\n', start)) !== -1) {
-                    const event = buffer.slice(start, end).trim();
-                    if (event.startsWith('data: ')) {
-                        const line = event.slice(6).trim();
-                        if (line.startsWith('{') && line.endsWith('}')) {
-                            const isDone = processEvent(line);
-                            if (isDone) {
-                                break;
-                            }
-                        }
-                    }
-                    start = end + 1;
+                if (message.finished) {
+                    eventSource.close(); // Close the connection when the response is finished
                 }
-                buffer = buffer.slice(start);
-
-                if (!done) {
-                    return reader.read().then(processText);
-                }
-            }
-
-            return reader.read().then(processText);
+            };
         });
+
+    function handleNewMessage(contentElement, message) {
+        const paragraph = document.createElement('p');
+        paragraph.textContent = message;
+        contentElement.appendChild(paragraph);
+    }
 });
